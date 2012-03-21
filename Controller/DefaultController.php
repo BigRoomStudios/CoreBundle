d1010f1cec5768489179a673504da65c55ddf9a5
Sam Mateosian <sam@bigroomstudios.com>
2012-03-21 13:45:25 -0400

    adding default controller to core

---
+++ Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BRS\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * DefaultController for core is a placeholder
+
+ */
+class DefaultController extends Controller
+{
+	
+}
