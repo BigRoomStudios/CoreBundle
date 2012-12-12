f194d0552a8aa108a7575e88b11c442080513fec
Sam Mateosian <sam@bigroomstudios.com>
2012-12-12 17:06:48 +0000

    added file_size filter to twig

--- Twig/TwigExtension.php
+++ Twig/TwigExtension.php
@@ -3,24 +3,31 @@
 namespace BRS\CoreBundle\Twig;
 
 class TwigExtension extends \Twig_Extension {
-
+	
+	public function getName(){
+		
+        return 'brs_twig_extension';
+    }
+	
     public function getFilters() {
+    	
         return array(
             'var_dump'   => new \Twig_Filter_Function('var_dump'),
             'highlight'  => new \Twig_Filter_Method($this, 'highlight'),
             'print_pre'  => new \Twig_Filter_Method($this, 'print_pre'),
+            'file_size'  => new \Twig_Filter_Method($this, 'file_size'),
         );
     }
 	
-	public function getFunctions()
-    {
+	public function getFunctions(){
+		
         return array(
             'update'  => new \Twig_Function_Method($this, 'update'),
         );
     }
 	
-	public function getTests()
-    {
+	public function getTests(){
+		
         return array(
             'numeric' => new \Twig_Test_Method($this, 'numeric'),
         );
@@ -34,6 +41,7 @@ class TwigExtension extends \Twig_Extension {
 	}
 	
     public function highlight($sentence, $expr) {
+    	
         return preg_replace('/(' . $expr . ')/', '<span style="color:red">\1</span>', $sentence);
     }
 	
@@ -44,14 +52,16 @@ class TwigExtension extends \Twig_Extension {
         return '<pre>' . $print . '</pre>';
     }
 
-    public function getName()
-    {
-        return 'brs_twig_extension';
-    }
-	
 	public function numeric($value){
 		
 		return is_numeric($value);
 	}
 
+	public function file_size($value){
+		
+		if ($value >= 1073741824) { return round($value / 1073741824, 2) . ' GB'; }
+		if ($value >= 1048576)    { return round($value / 1048576, 2) . ' MB'; }
+		if ($value >= 1024)       { return round($value / 1024, 0) . ' KB'; }
+		return $value . ' B';
+	}
 }
